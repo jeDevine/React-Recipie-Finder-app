b8472a93be0f802dae091f4eebb5ea7b8f195f03
james <jamesedwarddevine@gmail.com>
2022-01-21 15:01:12 -0500

    update

--- src/components/SearchForms.tsx
+++ src/components/SearchForms.tsx
@@ -1,2 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Recipe } from '../models/recipe';
+import { fetchAllData } from '../services/RecipieAPI';
+import SearchCal from './SearchCal'
 
+const SearchForm = () => {
+    const [recipe, setRecipe] = useState<Recipe[]>([]);
+    const [calorieChoice, setCaloriesChoice] = useState("");
+
+    useEffect(()=>{
+        if (calorieChoice){
+        }
+    }, [calorieChoice])
+
+    const findByCalories = (cal: number) => {
+
+    }
+
+    return (
+        <div>
+            <SearchCal onSubmit={findByCalories}/>
+        </div>
+    )
+}
