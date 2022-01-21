2c53a6c0b4d7fb6fffbbe5ee5fadedc0cba703ed
james <jamesedwarddevine@gmail.com>
2022-01-21 16:18:33 -0500

    update

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import SearchForms from './SearchForms';
 
 const Main = () => {
     const [] = useState();
+
+    const handleSubmitForm = () => {
+
+    }
+
     return (
         <div>
-        {/* <SearchForms />
-        <ResultsList /> */}
+            <h1>Recipe Rescue!</h1>
+            <SearchForms onSubmit={handleSubmitForm}/>
+            {/* <ResultsList /> */}
         </div>
     )
 }
--- src/components/SearchForms.tsx
+++ src/components/SearchForms.tsx
@@ -1,24 +1,84 @@
-import React, { useEffect, useState } from 'react'
+import React, { FormEvent, useEffect, useState } from 'react'
 import { Recipe } from '../models/recipe';
 import { fetchAllData } from '../services/RecipieAPI';
-import SearchCal from './SearchCal'
 
-const SearchForm = () => {
+
+interface Prop {
+    onSubmit: (recipe: Recipe) => void;
+}
+
+const SearchForms = ({onSubmit}: Prop) => {
     const [recipe, setRecipe] = useState<Recipe[]>([]);
-    const [calorieChoice, setCaloriesChoice] = useState("");
+    const [calories, setCalories] = useState("");
+    const [label, setLabel] = useState("");
+    const [totalTime, setTotalTime] = useState("");
 
-    useEffect(()=>{
-        if (calorieChoice){
+
+    const handleSubmit = (e:FormEvent) => {
+        e.preventDefault();
+        let results: Recipe = {
+            label: label,
+            healthLabel: [],
+            calories: parseInt(calories),
+            image: '',
+            url: '',
+            totalTime: parseInt(totalTime),
+            ingredients: []
         }
-    }, [calorieChoice])
-
-    const findByCalories = (cal: number) => {
-
+        
+        onSubmit(results);
     }
-
+    
     return (
         <div>
-            <SearchCal onSubmit={findByCalories}/>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="calories">Search By Max Calorie Count<input type="number" name="calories" value={calories} onChange={(e) => setCalories(e.target.value)} min="0" step="1"/></label>
+                <label htmlFor="label">Search By Name<input type="text" name="label" value={label} onChange={(e) => setLabel(e.target.value)}/></label>
+                <label htmlFor="totalTime">Search By Time to Cook (minutes)<input type="number" name="totalTime" value={totalTime} onChange={(e) => setTotalTime(e.target.value)}/></label>
+                <select name="health-restrictions" id="health-dropdown">
+                    <option value="alcohol-cocktail"> alcohol-cocktail  </option>
+                    <option value="alcohol-free"> alcohol-free  </option>
+                    <option value="celery-free"> celery-free  </option>
+                    <option value="crustacean-free"> crustacean-free  </option>
+                    <option value="dairy-free"> dairy-free  </option>
+                    <option value="DASH"> DASH  </option>
+                    <option value="egg-free"> egg-free  </option>
+                    <option value="fish-free"> fish-free  </option>
+                    <option value="fodmap-free"> fodmap-free  </option>
+                    <option value="gluten-free"> gluten-free  </option>
+                    <option value="immuno-supportive"> immuno-supportive  </option>
+                    <option value="keto-friendly"> keto-friendly  </option>
+                    <option value="kidney-friendly"> kidney-friendly  </option>
+                    <option value="kosher"> kosher  </option>
+                    <option value="low-fat-abs"> low-fat-abs  </option>
+                    <option value="low-potassium"> low-potassium  </option>
+                    <option value="low-sugar"> low-sugar  </option>
+                    <option value="lupine-free"> lupine-free  </option>
+                    <option value="Mediterranean"> Mediterranean  </option>
+                    <option value="mollusk-free"> mollusk-free  </option>
+                    <option value="mustard-free"> mustard-free  </option>
+                    <option value="no-oil-added"> no-oil-added  </option>
+                    <option value="paleo"> paleo  </option>
+                    <option value="peanut-free"> peanut-free  </option>
+                    <option value="pescatarian"> pescatarian  </option>
+                    <option value="pork-free"> pork-free  </option>
+                    <option value="red-meat-free"> red-meat-free  </option>
+                    <option value="sesame-free"> sesame-free  </option>
+                    <option value="shellfish-free"> shellfish-free  </option>
+                    <option value="soy-free"> soy-free  </option>
+                    <option value="sugar-conscious"> sugar-conscious  </option>
+                    <option value="sulfite-free"> sulfite-free  </option>
+                    <option value="tree-nut-free"> tree-nut-free  </option>
+                    <option value="vegan"> vegan  </option>
+                    <option value="vegetarian"> vegetarian  </option>
+                    <option value="wheat-free"> wheat-free  </option>
+                </select>
+                <button type="submit">Search</button>
+            </form>
         </div>
     )
 }
+
+
+
+export default SearchForms;
